c302fff9653b07282bf4754af35b986295ffea04
Bacal Francis <francisbacal@gmail.com>
2020-05-13 09:34:39 +0800

    css mix for initial deployment

--- webpack.mix.js
+++ webpack.mix.js
@@ -13,3 +13,10 @@ const mix = require('laravel-mix');
 
 mix.js('resources/js/app.js', 'public/js')
     .sass('resources/sass/app.scss', 'public/css');
+
+mix.styles([
+    'public/css/adminlte.css',
+    'public/plugins/fontawesome-free/css/all.min.css',
+    'public/plugins/overlayScrollbars/css/OverlayScrollbars.min.css',
+    'public/css/app.css'
+], 'public/css/all.css');
